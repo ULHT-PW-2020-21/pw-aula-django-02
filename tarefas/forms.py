1619164e86042c045a2d0e0706b4ab5a1b152636
Lucio Studer Ferreira <42048382+luciostuder@users.noreply.github.com>
2023-05-16 11:36:58 +0100

    Update forms.py

--- tarefas/forms.py
+++ tarefas/forms.py
@@ -7,21 +7,24 @@ class TarefaForm(ModelForm):
     class Meta:
         model = Tarefa
         fields = '__all__'
-    # inserção de classes CSS para formatação de cada campo do formulário
+   
+        # Para um conjunto de propriedade da classe (titulo, prioridade, concluido, etc), 
+        # o dicionário widgets permite configurar o elemento HTML input correspondente, 
+        # através de um dicionario de atributos de formatação (especificação de classes, placeholder, propriedades, etc).
         widgets = {
             'titulo': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'descrição da tarefa...'}),
             'prioridade': forms.NumberInput(attrs={'class': 'form-control', 'max': 3, 'min': 1}),
         }
 
 
-    # texto a exibir junto à janela de inserção
+       # o dicionário labels especifica o texto a exibir junto à janela de inserção
         labels = {
             'titulo': 'Título',
             'concluido': 'Concluída',
         }
 
 
-    # texto auxiliar a um determinado campo do formulário
+       # o dicionário help_texts contém, para um atributo, um texto auxiliar a apresentar por baixo da janela de inserção
         help_texts = {
             'prioridade': 'prioridade: baixa=1, media=2, alta=3',
         }
